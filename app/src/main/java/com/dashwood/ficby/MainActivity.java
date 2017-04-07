ea6be8a5a63e041cb7779f9be1d13b7d26e81b00
Pin-Lun <pudchi330@gmail.com>
2017-04-07 16:46:17 +0800

    Button Customize

--- app/src/main/java/com/dashwood/ficby/MainActivity.java
+++ app/src/main/java/com/dashwood/ficby/MainActivity.java
@@ -1,18 +1,27 @@
 package com.dashwood.ficby;
 
+
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView app_logo;
+    Button account, beacon, band, more;
+
+    private String CIRCULAR_BOLD = "Circular_bold.otf";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        app_logo = (TextView) findViewById(R.id.app_logo);
+        account = (Button) findViewById(R.id.account_button);
 
 
     }
