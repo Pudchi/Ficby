d5ac99f96b50d0e2f7dcab066e4f433c362e82b8
Pin-Lun <pudchi330@gmail.com>
2017-04-08 15:50:41 +0800

    Add Login Activity (Conbined with Firebase) and Specify App icon

--- app/src/main/java/com/dashwood/ficby/MainActivity.java
+++ app/src/main/java/com/dashwood/ficby/MainActivity.java
@@ -12,17 +12,31 @@ public class MainActivity extends AppCompatActivity {
     TextView app_logo;
     Button account, beacon, band, more;
 
-    private String CIRCULAR_BOLD = "Circular_bold.otf";
+    String CIRCULAR_BOLD = "fonts/Circular_bold.ttf";
+    String CIRCULAR_BOOK = "fonts/Circular_book.ttf";
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface typeface_bold = Typeface.createFromAsset(getAssets(), CIRCULAR_BOLD);
+        Typeface typeface_book = Typeface.createFromAsset(getAssets(), CIRCULAR_BOOK);
+
 
         app_logo = (TextView) findViewById(R.id.app_logo);
-        account = (Button) findViewById(R.id.account_button);
+        app_logo.setTypeface(typeface_bold);
 
+        account = (Button) findViewById(R.id.account_button);
+        account.setTypeface(typeface_book);
+        beacon = (Button) findViewById(R.id.beacon_button);
+        beacon.setTypeface(typeface_book);
+        band = (Button) findViewById(R.id.band_button);
+        band.setTypeface(typeface_book);
+        more = (Button) findViewById(R.id.more_button);
+        more.setTypeface(typeface_book);
 
     }
 
