fa8961b771615644b6607264f0510dcc40288512
Pin-Lun <pudchi330@gmail.com>
2017-04-08 18:36:55 +0800

    Login. Sign up. Forget App view

--- app/src/main/java/com/dashwood/ficby/MainActivity.java
+++ app/src/main/java/com/dashwood/ficby/MainActivity.java
@@ -1,42 +1,54 @@
 package com.dashwood.ficby;
 
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView app_logo;
+    TextView app_logo, version;
     Button account, beacon, band, more;
 
-    String CIRCULAR_BOLD = "fonts/Circular_bold.ttf";
-    String CIRCULAR_BOOK = "fonts/Circular_book.ttf";
+    static final String CIRCULAR_BOLD = "fonts/Circular_bold.ttf";
+    static final String CIRCULAR_BOOK = "fonts/Circular_book.ttf";
 
-
+    Typeface typeface_bold;
+    Typeface typeface_regular;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Typeface typeface_bold = Typeface.createFromAsset(getAssets(), CIRCULAR_BOLD);
-        Typeface typeface_book = Typeface.createFromAsset(getAssets(), CIRCULAR_BOOK);
+        typeface_bold = Typeface.createFromAsset(getAssets(), CIRCULAR_BOLD);
+        typeface_regular = Typeface.createFromAsset(getAssets(), CIRCULAR_BOOK);
 
 
         app_logo = (TextView) findViewById(R.id.app_logo);
         app_logo.setTypeface(typeface_bold);
+        version = (TextView) findViewById(R.id.version);
+        version.setTypeface(typeface_bold);
 
         account = (Button) findViewById(R.id.account_button);
-        account.setTypeface(typeface_book);
+        account.setTypeface(typeface_regular);
         beacon = (Button) findViewById(R.id.beacon_button);
-        beacon.setTypeface(typeface_book);
+        beacon.setTypeface(typeface_regular);
         band = (Button) findViewById(R.id.band_button);
-        band.setTypeface(typeface_book);
+        band.setTypeface(typeface_regular);
         more = (Button) findViewById(R.id.more_button);
-        more.setTypeface(typeface_book);
+        more.setTypeface(typeface_regular);
+
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
 
     }
 
--- app/src/main/java/com/dashwood/ficby/SignupActivity.java
+++ app/src/main/java/com/dashwood/ficby/SignupActivity.java
@@ -1,58 +1,41 @@
 package com.dashwood.ficby;
 
-import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import static com.dashwood.ficby.MainActivity.CIRCULAR_BOLD;
 import static com.dashwood.ficby.MainActivity.CIRCULAR_BOOK;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
-    //TextView app_logo;
-    Button login, forget, sign_up;
+    Button backto_login, sign_up;
     EditText input_mail, input_password;
     Typeface typeface_bold, typeface_regular;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
+
         typeface_bold = Typeface.createFromAsset(getAssets(), CIRCULAR_BOLD);
         typeface_regular = Typeface.createFromAsset(getAssets(), CIRCULAR_BOOK);
-
-        /*app_logo = (TextView) findViewById(R.id.app_logo);
-        app_logo.setTypeface(typeface_bold);*/
 
         input_mail = (EditText) findViewById(R.id.email_input);
         input_mail.setTypeface(typeface_regular);
         input_password = (EditText) findViewById(R.id.password_input);
         input_password.setTypeface(typeface_regular);
-
-        login = (Button) findViewById(R.id.btn_login);
-        forget = (Button) findViewById(R.id.btn_forget);
         sign_up = (Button) findViewById(R.id.btn_signup);
+        backto_login = (Button) findViewById(R.id.btn_backtologin);
 
-        sign_up.setOnClickListener(new View.OnClickListener() {
+        backto_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+                finish();
             }
         });
-
-        forget.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, ForgetPasswordActivity.class));
-            }
-        });
-
-
     }
 }
