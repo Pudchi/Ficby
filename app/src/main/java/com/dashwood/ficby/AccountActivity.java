f7edb468d0c37dc65f8bf4231c4c9a5f0c03963c
Pin-Lun <pudchi330@gmail.com>
2017-04-29 17:00:47 +0800

    Revise Account Activity & Add Animation

--- app/src/main/java/com/dashwood/ficby/AccountActivity.java
+++ app/src/main/java/com/dashwood/ficby/AccountActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -18,12 +19,13 @@ import static com.dashwood.ficby.MainActivity.SOFT_MEDIUM;
 public class AccountActivity extends AppCompatActivity {
 
     Button log_out, backto_main;
-    TextView account_mail;
+    TextView account_mail, account_header;
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth auth;
     private FirebaseUser user;
     Typeface typeface_regular, typeface_zh_medium;
-    String mail_address ="";
+    //String mail_address ="";
+    LottieAnimationView user_fingerprint_anim, circle_anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +52,12 @@ public class AccountActivity extends AppCompatActivity {
         log_out = (Button) findViewById(R.id.btn_logout);
         log_out.setTypeface(typeface_zh_medium);
         account_mail = (TextView) findViewById(R.id.account_text);
+        account_header = (TextView) findViewById(R.id.account_header);
+        account_header.setTypeface(typeface_zh_medium);
         backto_main = (Button) findViewById(R.id.btn_backto_main);
         backto_main.setTypeface(typeface_zh_medium);
+        user_fingerprint_anim = (LottieAnimationView) findViewById(R.id.user_animation);
+        circle_anim = (LottieAnimationView) findViewById(R.id.circle_animation);
 
         //Intent intent = this.getIntent();
         //String mail_address = intent.getStringExtra("account_mail");
@@ -79,11 +85,21 @@ public class AccountActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        circle_anim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AccountActivity.this, MoreActivity.class));
+                finish();
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        user_fingerprint_anim.playAnimation();
+        circle_anim.playAnimation();
     }
 
     @Override
@@ -98,6 +114,8 @@ public class AccountActivity extends AppCompatActivity {
         if (authListener != null) {
             auth.removeAuthStateListener(authListener);
         }
+        user_fingerprint_anim.cancelAnimation();
+        circle_anim.cancelAnimation();
     }
 
     @Override
@@ -107,5 +125,7 @@ public class AccountActivity extends AppCompatActivity {
         if (authListener != null) {
             auth.removeAuthStateListener(authListener);
         }
+        user_fingerprint_anim.cancelAnimation();
+        circle_anim.cancelAnimation();
     }
 }
